b07b8bb1e02051e2dafb23ef6d4aef9e2332b795
Dinçer Yiğit <dinceryigit60@gmail.com>
2022-11-05 06:41:03 +0300

    Add project files.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,60 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        private ICarDal _carDal;
+        public CarManager(ICarDal carDal)
+        {
+            _carDal=carDal;
+        }
+        public void Add(Car car)
+        {
+            if (car.Descriptionn.Length >= 2 && car.DailyPrice > 0)
+            {
+                _carDal.Add(car);
+            }
+            else if(car.Descriptionn.Length >= 2 && car.DailyPrice < 0)
+            {
+                Console.WriteLine("Gunluk fiyat sifirdan kucuk ");
+            }
+            else if(car.Descriptionn.Length < 2 && car.DailyPrice > 0)
+            {
+                Console.WriteLine("Araba aciklamasi 2 karakterden kucuk");
+            }
+        }
+
+        public void Delete(Car car)
+        {
+            _carDal.Delete(car);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _carDal.GetAll();
+        }
+
+        public List<Car> GetCarsByBrandId(int brandId)
+        {
+            return _carDal.GetAll(c => c.BrandId == brandId);
+        }
+
+        public List<Car> GetCarsByColorId(int colorId)
+        {
+            return _carDal.GetAll(c=> c.ColorId == colorId);
+        }
+
+        public void Update(Car car)
+        {
+            _carDal.Update(car);
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramwork;
+using Entities.Concrete;
+
+
+
+
+CarManager carManager = new CarManager(new EfCarDal());
+
+//carManager.Add(new Car { Id=1,BrandId = 1, ColorId = 2, DailyPrice = 1591, Descriptionn = "Mercedes C Serisi", ModelYear = 2019 });
+//carManager.Add(new Car { Id=2,BrandId = 4, ColorId = 3, DailyPrice = 1647, Descriptionn = "Volvo XC40", ModelYear = 2020 });
+//carManager.Add(new Car { Id = 3, BrandId = 5, ColorId = 7, DailyPrice = 1988, Descriptionn = "Audi TT", ModelYear = 2021 });
+
+foreach (var car in carManager.GetAll()) 
+{
+    Console.WriteLine(car.Descriptionn);
+}
